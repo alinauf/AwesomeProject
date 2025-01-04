1c20e370f70629bf70fa1657799b263292455f1c
Nauf <naufpsychic@gmail.com>
2025-01-04 18:47:14 +0500

    fix(RN73.3): add wix react native navigation
    Fix: RN73.3
    https://github.com/wix/react-native-navigation/issues/7821#issuecomment-2514579985

--- index.js
+++ index.js
@@ -5,5 +5,57 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {Navigation} from 'react-native-navigation';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+// In index.js of a new project
+
+const HomeScreen = props => {
+  return (
+    <View style={styles.root}>
+      <Text>Hello React Native Navigation 👋</Text>
+    </View>
+  );
+};
+HomeScreen.options = {
+  topBar: {
+    title: {
+      text: 'Home',
+      color: 'white',
+    },
+    background: {
+      color: '#4d089a',
+    },
+  },
+};
+
+Navigation.registerComponent('Home', () => HomeScreen);
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
+  },
+});
